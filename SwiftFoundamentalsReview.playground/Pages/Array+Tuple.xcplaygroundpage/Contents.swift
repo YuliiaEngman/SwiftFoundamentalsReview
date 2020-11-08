ed323264111b5aa6e1336c8b2cc15d7fb89ad11c
Yuliia Engman <yuliia.engman@gmail.com>
2020-11-07 22:22:39 -0500

    continue

--- SwiftFoundamentalsReview.playground/Pages/Array+Tuple.xcplaygroundpage/Contents.swift
+++ SwiftFoundamentalsReview.playground/Pages/Array+Tuple.xcplaygroundpage/Contents.swift
@@ -13,6 +13,20 @@ func bestProfessionals(pros: [[Int]], k: Int) -> [Int] {
     }
     
     // Step 2: Calculate for every professional their PMS
+    for tuple in pros {
+        if pros[0] == 0 {
+          let prefessional1PMS = (maxDistance - tuple[0]) * tuple[1]
+        } if pros[1] == 1 {
+           let prefessional2PMS = (maxDistance - tuple[0]) * tuple[1]
+        } if pros[2] == 2 {
+            let prefessional3PMS = (maxDistance - tuple[0]) * tuple[1]
+        } if pros[3] == 3 {
+            let prefessional4PMS = (maxDistance - tuple[0]) * tuple[1]
+        }
+    }
     
-
+    var bestPMSfromProfessionals = [prefessional1PMS, prefessional2PMS, prefessional3PMS,prefessional4PMS]
+    bestProfessionals.sorted()
+    
+    
 }
